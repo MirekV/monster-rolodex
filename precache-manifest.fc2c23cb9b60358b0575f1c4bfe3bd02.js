f74d56270ad93035d399bb4984d0fd1ee4a7cbfe
Miroslav Veis <miroslav.veis@email.cz>
2019-10-10 23:02:19 +0200

    Updates

---
+++ precache-manifest.fc2c23cb9b60358b0575f1c4bfe3bd02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bace09d4e08be32cb4421c17eff03976",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "de3fe533464f9bfe8bc4",
+    "url": "/monster-rolodex/static/css/main.7095975e.chunk.css"
+  },
+  {
+    "revision": "56022eb4a5dc281792e1",
+    "url": "/monster-rolodex/static/js/2.66e5ab65.chunk.js"
+  },
+  {
+    "revision": "de3fe533464f9bfe8bc4",
+    "url": "/monster-rolodex/static/js/main.85220ab7.chunk.js"
+  },
+  {
+    "revision": "6f70f97b8cfdb2d06998",
+    "url": "/monster-rolodex/static/js/runtime-main.aec4d7d7.js"
+  }
+]);
